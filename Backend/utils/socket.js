4ef70368522526011f0a5b531d58eff187261d79
Ankur Singh <ankur.singhh31@gmail.com>
2026-05-20 02:16:53 +0530

    adding the backend code of socket in socket.js

--- Backend/utils/socket.js
+++ Backend/utils/socket.js
@@ -1,4 +1,9 @@
 const Socket = require("socket.io");
+const crypto = require("crypto");
+
+const generateRoomId = (userId, targetUserId) => {
+    return crypto.createHash("sha256").update([userId, targetUserId].sort().join("_")).digest("hex");
+};
 
 const initializeSocket = (server) => {
     const io = Socket(server, {
@@ -10,20 +15,25 @@ const initializeSocket = (server) => {
 
     io.on("connection", (socket) => {
         // Handle Events
-        socket.on("joinChat", () => {
-
+        socket.on("joinChat", ({ firstname, userId, targetUserId }) => {
+            const roomId = generateRoomId(userId, targetUserId);
+            console.log(firstname + "joined room" + roomId);
+            socket.join(roomId);
         });
 
-        socket.on("sendMessage", () => {
-
+        socket.on("sendMessage", ({firstname, message, userId, targetUserId}) => {
+            const roomId = generateRoomId(userId, targetUserId);
+            io.to(roomId).emit("receiveMessage", { firstname, message, userId });
         });
 
-        socket.on("typing", () => {
-
+        socket.on("typing", ({ userId, targetUserId }) => {
+            const roomId = generateRoomId(userId, targetUserId);
+            io.to(roomId).emit("userTyping", { userId });
         });
 
-        socket.on("disconnect", () => {
-
+        socket.on("disconnect", ({ userId, targetUserId }) => {
+            const roomId = generateRoomId(userId, targetUserId);
+            io.to(roomId).emit("userDisconnected", { userId });
         });
 
     });
